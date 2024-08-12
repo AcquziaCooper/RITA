18a1219915f8471d02991f657ca496272593749d
AcquziaCooper <lungiekunene@gmail.com>
2024-08-12 19:47:08 +0200

    Added apiKey and url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "faa1b7a84c09e076f6304e2co3ba683t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input");
   let cityElement = document.querySelector("#app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search");
