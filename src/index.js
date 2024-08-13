a8e197ec72af5c6eb8fd8203d630bd028eb34100
AcquziaCooper <lungiekunene@gmail.com>
2024-08-13 18:31:15 +0200

    Did the search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "faa1b7a84c09e076f6304e2co3ba683t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
